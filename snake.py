7b94d9bfe78265f24047af50b1de67545fe9428a
wiewior945 <luki238@vp.pl>
2020-08-14 22:43:03 +0200

    implement PyEnvironment interface, environment is ready to use

--- snake.py
+++ snake.py
@@ -1,62 +1,84 @@
 import pygame
 import random
+import numpy as np
 
 from direction import Direction
 from tf_agents.environments import py_environment
+from tf_agents.specs import array_spec
+from tf_agents.trajectories import time_step as ts
 
 
 class Snake(py_environment.PyEnvironment):
     
-    speed = 500 #przerwa pomiędzu ruchami węża w ms (szybkosć węża)
+    speed = 500  # przerwa pomiędzu ruchami węża w ms (szybkosć węża)
     done = False
-    isPointAvaiable = False
-    #RGB colors:
+    # RGB colors:
     colorWhite = (255, 255, 255)
     colorBlack = (0, 0, 0)
     colorRed = (255, 0, 0)
     rectSize = 30
     rectMargin = 3
-    boardSize = 21  #number of printed rectangles in single line
+    boardSize = 21  # number of printed rectangles in single line
     windowSize = (boardSize*rectSize) + (boardSize*rectMargin) + rectMargin
     board = []
-    #head and tail are tuples which holds board indexes of snake's head and tail
-    snake = [[10,11], [10,10], [10,9]]
-    point = []  #czerwony punkt
+    point = []  # czerwony punkt
     direction = Direction.east
-    rectangle = pygame.Surface((100, 100))
     screen = pygame.display.set_mode((windowSize, windowSize))
     
     
-    #----------------------------------------------------------------------------------------------
+
     def __init__(self):
-        print("poszlo")
-        self.initBoard()
-        print("1")
-        self.initSnake()
-        print("2")
-        pygame.init()
-        print("3")
+        self._action_spec = array_spec.BoundedArraySpec(shape=(), dtype=np.int32, minimum=0, maximum=2, name='action')
+        self._observation_spec = array_spec.BoundedArraySpec(shape=(5, 2), dtype=np.int32, minimum=0, name='observation')
+        self._reset()
+        # to na dole to jakby się chciało ręcznie poklikać
+        # pygame.init()
+        # while(True):
+        #     inp = input()
+        #     action = np.array(inp, dtype=np.int32)
+        #     self._step(action)
+        #     pygame.display.flip()
+        #     pygame.time.delay(1000)
         
-        while not self.done:
-                for event in pygame.event.get():
-                        if event.type == pygame.QUIT:
-                            self.done = True       
-                pressed = pygame.key.get_pressed()
-                if pressed[pygame.K_UP]:
-                    self.direction=Direction.north
-                elif pressed[pygame.K_DOWN]:
-                    self.direction=Direction.south
-                elif pressed[pygame.K_LEFT]:
-                    self.direction=Direction.west
-                elif pressed[pygame.K_RIGHT]:
-                    self.direction=Direction.east
-                    
-                if(pygame.time.get_ticks()%self.speed==0): #warunek jest spełniany co sekundę
-                    pygame.time.delay(1)    #usypia program na 1 milisekundę, musiałem to dodać bo czasem w ciągu jednej milisekundy pętla potrafiła wykonać się dwa razy i szły dwa ruchy węża na raz.
-                    self.colision()
-                    if not self.isPointAvaiable: self.randomPoint()
-                    pygame.display.flip()
+    def action_spec(self):
+        return self._action_spec
     
+    def observation_spec(self):
+        return self._observation_spec
+    
+    def _reset(self):
+        self.done = False
+        self.board = []
+        self.direction = Direction.east
+        self.initBoard()
+        self.initSnake()
+        self.randomPoint()
+        observation = np.array([[0, 0], [1, 0], [2, 0], [10, 11], self.point], dtype=np.int32)
+        return ts.restart(observation)
+
+    # Action: 0 = nic, 1 = w lewo, 2 = w prawo
+    def _step(self, action):
+        print("--------------------------------------------")
+        print("Action: " + str(action))
+        if(self.done):  # jeśli tutaj jest true to oznacza, że w poprzednim kroku było zderzenie i jest reset gry
+            print("--- RESET ---")
+            return self._reset()
+        if(action!=0):  # jeśli coś innego od 0 to agent chce zmienić kierunek węża
+            self.changeDirection(action)
+        print("Direction: " + str(self.direction))
+        colisionOutput = self.colision() # przesunięcie węża, zwraca otoczenie głowy[0], głowę[1] i nagrodę[2]
+        observation = colisionOutput[0]
+        observation = np.vstack((observation, colisionOutput[1]))
+        observation = np.vstack((observation, self.point))
+        reward = colisionOutput[2]
+        print("Reward: " + str(reward))
+        print("Head: " + str(colisionOutput[1]))
+        print("Observation: " + str(observation))
+        if(self.done):  # true jest jeśli doszło do zderzenia
+            return ts.termination(observation, reward)
+        else:
+            return ts.transition(observation, reward)
+
     
     # tworzy planszę, board to odpowiednik dwuwymiarowej tablicy z obiektami kwadratów
     # druga pętla (j) tworzy poziomy rząd kwadratów, pierwsza pętla dodaje poziomy rząd do głównej tablicy planszy
@@ -73,50 +95,166 @@ class Snake(py_environment.PyEnvironment):
             
     
     def initSnake(self):
+        self.snake = [[10,11], [10,10], [10,9]]
         pygame.draw.rect(self.screen, self.colorBlack, self.board[self.snake[0][0]][self.snake[0][1]])
         pygame.draw.rect(self.screen, self.colorBlack, self.board[self.snake[1][0]][self.snake[1][1]])
         pygame.draw.rect(self.screen, self.colorBlack, self.board[self.snake[2][0]][self.snake[2][1]])
-        
-    
+
+    # action 1 = lewo, action 2 = prawo
+    def changeDirection(self, action):
+        if(self.direction == Direction.east):
+            if(action == 1):
+                self.direction = Direction.north
+                print("zmiana")
+            elif(action == 2):
+                self.direction = Direction.south
+                print("zmiana")
+        elif(self.direction == Direction.south):
+            if(action == 1):
+                self.direction = Direction.east
+                print("zmiana")
+            elif(action == 2):
+                self.direction = Direction.west
+                print("zmiana")
+        elif(self.direction == Direction.west):
+            if(action == 1):
+                self.direction = Direction.south
+                print("zmiana")
+            elif(action == 2):
+                self.direction = Direction.north
+                print("zmiana")
+        elif(self.direction == Direction.north):
+            if(action == 1):
+                self.direction = Direction.west
+                print("zmiana")
+            elif(action == 2):
+                self.direction = Direction.east
+                print("zmiana")
+
+
+    # rysuje głowę w nowym miejscu i ucina ogon lub go zostawia jeśli nowa głowa jest równa jabłku
     def move(self, head, addPoint):
-        if addPoint:
-            addPoint = False
-            self.isPointAvaiable = False
-        else:
+        if not addPoint:  # jeśli zjadł jabłko to nie ucinam ogona
             tail = self.snake[len(self.snake)-1][:]   # [:] oznacza, że kopiujemy wartosc obiektu, a nie jego adres, a to jest tutaj ważne
             pygame.draw.rect(self.screen, self.colorWhite, self.board[tail[0]][tail[1]])
             self.snake.remove(tail)
-        self.snake.insert(0,head)
-        pygame.draw.rect(self.screen, self.colorBlack, self.board[head[0]][head[1]])
-       
+        self.snake.insert(0,head)  # dodaje do węża głowę w nowym miejscu
+        pygame.draw.rect(self.screen, self.colorBlack, self.board[head[0]][head[1]]) #rysuje głowę w nowym miejscu
+        if(addPoint):
+            self.randomPoint()
     
     def randomPoint(self):
-        while not self.isPointAvaiable:
+        isPointAvailable = False
+        while not isPointAvailable:
             x = random.randint(0, self.boardSize-1)
             y = random.randint(0, self.boardSize-1)
-            if self.snake.count([x,y])==0:
+            if self.snake.count([x, y])==0:
                 pygame.draw.rect(self.screen, self.colorRed, self.board[x][y])
-                self.isPointAvaiable = True    
+                isPointAvailable = True
                 self.point = [x,y]
-                
+
+
+    #zwraca: otoczenie głowy (w którą stronę można wykonać ruch bez kolizjii), głowę i nagrodę
     def colision(self):
         addPoint = False
-        head = self.snake[0][:]
+        reward = 0
+        head = self.snake[0][:]  # snake[0] to głowa węża. [:] kopiuje wartość obiektu, a nie jego adres. Head jest nowym obiektem z taką samą wartością jak głowa węża.
+        surroundings = np.array([99,99], dtype=np.int32) #te wartości 99 są tylko do stworzenia tablicy i nadania jej odpowiedniego shape, bez tego tablica nie chciała się zainicjalizować. Na końcu metody te wartości są usunięte
+
+        # poniżej przesuwa nową głowę odpowiednio do obecnego kierunku
+        # ustala nagrodę w zależności od zbliżania się do jabłka lub oddalania się od niego
         if(self.direction==Direction.east):
-            head[1]+=1
+            head[1] += 1
+            if(self.point[1] >= head[1]):
+                reward = 5
+            elif(self.point[1] < head[1]):
+                reward = -5
+            right = [head[0]+1, head[1]]
+            left = [head[0]-1, head[1]]
+            front = [head[0], head[1]+1]
+            if(self.snake.count(front) > 0 or front[1]>= self.boardSize):
+                surroundings = np.vstack((surroundings, [0, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [0, 0]))
+            if(self.snake.count(left) > 0 or left[0] < 0):
+                surroundings = np.vstack((surroundings, [1, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [1, 0]))
+            if(self.snake.count(right) > 0 or right[0] >= self.boardSize):
+                surroundings = np.vstack((surroundings, [2, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [2, 0]))
         elif(self.direction==Direction.north):
-            head[0]-=1
+            head[0] -= 1
+            if(self.point[0] <= head[0]):
+                reward = 5
+            elif(self.point[0] > head[0]):
+                reward = -5
+            front = [head[0] - 1, head[1]]
+            left = [head[0], head[1] - 1]
+            right = [head[0], head[1] + 1]
+            if(self.snake.count(front) > 0 or front[0] < 0):
+                surroundings = np.vstack((surroundings, [0, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [0, 0]))
+            if(self.snake.count(left) > 0 or left[1] < 0):
+                surroundings = np.vstack((surroundings, [1, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [1, 0]))
+            if(self.snake.count(right) > 0 or right[1] >= self.boardSize):
+                surroundings = np.vstack((surroundings, [2, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [2, 0]))
         elif(self.direction==Direction.west):
-            head[1]-=1
+            head[1] -= 1
+            if(self.point[1] <= head[1]):
+                reward = 5
+            elif(self.point[1] > head[1]):
+                reward = -5
+            front = [head[0], head[1]-1]
+            left = [head[0]+1, head[1]]
+            right = [head[0]-1, head[1]]
+            if(self.snake.count(front) > 0 or front[1] < 0):
+                surroundings = np.vstack((surroundings, [0, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [0, 0]))
+            if(self.snake.count(left) > 0 or left[0] >= self.boardSize):
+                surroundings = np.vstack((surroundings, [1, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [1, 0]))
+            if(self.snake.count(right) > 0 or right[0] < 0):
+                surroundings = np.vstack((surroundings, [2, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [2, 0]))
         elif(self.direction==Direction.south):
-            head[0]+=1
+            head[0] += 1
+            if(self.point[0] >= head[0]):
+                reward = 5
+            elif(self.point[0] < head[0]):
+                reward = -5
+            front = [head[0]+1, head[1]]
+            left = [head[0], head[1]+1]
+            right = [head[0], head[1]-1]
+            if(self.snake.count(front) > 0 or front[0] >= self.boardSize):
+                surroundings = np.vstack((surroundings, [0, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [0, 0]))
+            if(self.snake.count(left) > 0 or left[1] >= self.boardSize):
+                surroundings = np.vstack((surroundings, [1, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [1, 0]))
+            if(self.snake.count(right) > 0 or right[1] < 0):
+                surroundings = np.vstack((surroundings, [2, 1]))
+            else:
+                surroundings = np.vstack((surroundings, [2, 0]))
+        surroundings = np.delete(surroundings, 0, axis=0) #usunięcie pierwszego wiersza ([99, 99]), który był potrzebny do zainicjowania tablicy
         
-        if head==self.point:
+        if head==self.point:  # jeśli nowa głowa jest równa koordynatom jabłka
             addPoint = True
-        elif self.snake.count(head)>0:
+            reward = 100
+        # jeśli nowa głowa znajduje się w ciele węża lub jeśli wyjedzie poza planszę
+        elif (self.snake.count(head) > 0 or head[0] < 0 or head[1] >= self.boardSize or head[1] < 0 or head[1] >= self.boardSize):
             self.done = True
+            return surroundings, head, -1000  # kolizja więc nie potrzeba dalej rysować, zwraca -1000 punktów jako koniec gry
         self.move(head, addPoint)
-            
-    #-----------------------------------------------------------------------------------------------
-    
-s = Snake()
+        return surroundings, head, reward
